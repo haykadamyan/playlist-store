e438c4a822ba0ae94aec9ac23c0a8d3106e4821e
Aram <Simonyan>
2017-06-14 19:01:39 +0400

    Adding .ejs

--- app/server.js
+++ app/server.js
@@ -5,17 +5,21 @@ const Koa = require('koa');
 const render = require('koa-ejs');
 const Router = require('koa-router');
 const views = require('koa-views');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
+
+app.use(views(path.join(__dirname, '/view'), { extension: 'ejs' }));
+
 
 router.get('/', function(ctx){
     ctx.body = "SOLID"
 });
 
 
-router.get('/create-playlist', function(ctx){
-    app.use(views(path.join(__dirname, '/views'), { extension: 'ejs' }));
+router.get('/create-playlist', async function(ctx){
+    await ctx.render('create-playlist')
 });
 
 app.use(router.routes()).use(router.allowedMethods());
