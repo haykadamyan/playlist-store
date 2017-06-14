ec7bb857368ad2e90bfb2ee7b19d2c3f4d80ee4f
Armen Paronikyan <armparonikyan@gmail.com>
2017-06-14 18:58:21 +0400

    Implemented html render

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const http = require('http');
 const Koa = require('koa');
 const render = require('koa-ejs');
 const Router = require('koa-router');
+const views = require('koa-views');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', function(ctx){
-    ctx.body = "SOLID"
+app.use(views(__dirname + '/app/view',{extension: 'ejs'}));
+
+
+router.get('/', async function(ctx){
+    await ctx.render('main');
 });
 
 app.use(router.routes()).use(router.allowedMethods());
-console.log("Server running on localhost: 3000")
+console.log("Server running on localhost: 3000");
 app.listen(3000);
