28a957def3cb0a22f01f09e9bd9e49e04c2317b4
Artur Grigoryan <artur.grigoryan6@tumo.org>
2017-06-16 19:44:09 +0400

    header.ejs create and use static files

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const Koa = require('koa');
 const render = require('koa-ejs');
-
+const serve = require('koa-static');
 const views = require('koa-views');
 const path = require('path');
 
@@ -12,6 +12,6 @@ const app = new Koa();
 const router = require("./services/router.js");
 
 app.use(views(path.join(__dirname, '/view'), { extension: 'ejs' }));
-
+app.use(serve('./public'));
 app.use(router.routes()).use(router.allowedMethods());
 module.exports = app;
