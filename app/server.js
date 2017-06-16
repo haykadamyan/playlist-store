8a610d97e8e9cf2f69b06079f2481eacb1898c7c
Aram <Simonyan>
2017-06-16 18:15:16 +0400

    Adding .ejs

--- app/server.js
+++ app/server.js
@@ -27,12 +27,17 @@ router.get('/', async function(ctx){
 router.get('/payment', async function(ctx) {
     await ctx.render('payment', {payment: payment});
 });
+
 router.get('/playlist', async function(ctx){
     await ctx.render('playlist')
 });
 
 router.get('/playlist-page', async function(ctx){
     await ctx.render('playlist-page', {title: "Playlist page"})
+});
+
+router.get('/create-playlist', async function(ctx){
+    await ctx.render('create-playlist', {title: "Create playlist"})
 });
 
 app.use(router.routes()).use(router.allowedMethods());
