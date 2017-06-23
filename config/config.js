476231c6dd49273a67d0614fddae67d65623da78
Armen Paronikyan <armparonikyan@gmail.com>
2017-06-23 19:46:43 +0400

    Change secret and add force

--- config/config.js
+++ config/config.js
@@ -5,10 +5,11 @@ const config = {
     koaSecret: 'Some very strong key goes here, yo!',
     google: {
         clientID: '994707594746-124d3k6o679gs7m4iielmg86levi35sk.apps.googleusercontent.com',
-        clientSecret: 'evSSkF8i6_TvD4wgClYH_F1M',
-        callbackURL: 'http://play.am:3000/auth/youtube/callback',
+        clientSecret: 'MsZuGa41_OVOcU6GCHNAkL85',
+        callbackURL: 'http://playlist.am:3000/auth/youtube/callback',
         accessType: 'offline',
-        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/youtube']
+        scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/youtube'],
+        approvalPrompt: 'force'
     }
 };
 
